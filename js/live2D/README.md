868c0efe9910117b8e8db9dc79c9ae5d01c4b049
AiCyan <aicyan20@gmail.com>
2020-08-15 18:50:06 +0800

    上传文件

---
+++ js/live2D/README.md
@@ -0,0 +1,27 @@
+# Live2D Widget
+
+**特别说明：live2d-widget来源于[大神:Mimi](https://github.com/stevenjoezhang/live2d-widget)**
+
+
+## 特性
+
+在网页中添加 Live2D 看板娘。兼容 PJAX,支持无刷新加载、不再依赖于 jQuery。  
+Add Live2D widget to web page. Compatible with PJAX.
+**不支持 IE 11 等老旧浏览器。**
+
+
+## 示例 Demo
+
+>在[米米的博客](https://zhangshuqiao.org)的左下角可查看效果。(注：以下人物模型仅供展示之用，本仓库并不包含任何模型。)
+>
+>在[Ai的博客](https://cm.aicyan.top)的左下角可查看效果。
+
+## 引用
+
+**特别说明：这是一个快捷引用在页面上的方式。来源已在上方标明，如果要学习该项目请自行前往来源方学习。**
+
+将这一行代码加入 `<head>` 或 `<body>`，即可展现出效果：
+```xml
+<script src="https://cdn.jsdelivr.net/gh/AiCyan/img@latest/live2D/autoload.js"></script>
+```
+
---
+++ js/live2D/autoload.js
@@ -0,0 +1,128 @@
+// 参数应使用绝对路径
+const live2d_path = "https://cdn.jsdelivr.net/gh/AiCyan/img@latest/live2D/";
+
+// 封装异步加载资源的方法
+function loadExternalResource(url, type) {
+  return new Promise((resolve, reject) => {
+    let tag;
+    if (type === "css") {
+      tag = document.createElement("link");
+      tag.rel = "stylesheet";
+      tag.href = url;
+    } else if (type === "js") {
+      tag = document.createElement("script");
+      tag.src = url;
+    }
+    if (tag) {
+      tag.onload = () => resolve(url);
+      tag.onerror = () => reject(url);
+      document.head.appendChild(tag);
+    }
+  });
+}
+
+// 加载 js、css、json、api
+if (screen.width >= 10) {
+  Promise.all([
+    loadExternalResource(live2d_path + "waifu.css", "css"),
+    // loadExternalResource(live2d_path + "font-awesome.css", "css"),
+    loadExternalResource(live2d_path + "iconfont.css", "css"),
+    loadExternalResource(live2d_path + "live2d.min.js", "js"),
+    loadExternalResource(live2d_path + "waifu-tips.js", "js"),
+  ]).then(() => {
+    initWidget({
+      waifuPath: live2d_path + "waifu-tips.json",
+      apiPath: "https://live2d.fghrsh.net/api/",
+    });
+  });
+}
+
+// 拖拽
+window.onload = function () {
+  var getDiv = document.getElementById("waifu");
+  drag(getDiv);
+}
+
+function drag(node) {
+  var flag = false,
+    curX = 0,
+    curY = 0,
+    nodeX = 0,
+    nodeY = 0,
+    absX = 0,
+    absY = 0,
+    limX = 0,
+    limY = 0,
+    winW = document.documentElement.clientWidth || document.body.clientWidth,
+    winH = document.documentElement.clientHeight || document.body.clientHeight,
+    maxW = winW - node.offsetWidth,
+    maxH = winH - node.offsetHeight;
+
+  function down() {
+    flag = true;
+    var touch;
+    if (event.touches) {
+      touch = event.touches[0];
+    } else {
+      touch = event;
+    }
+    curX = touch.clientX;
+    curY = touch.clientY;
+    nodeX = node.offsetLeft;
+    nodeY = node.offsetTop;
+  }
+
+  function move() {
+    if (flag) {
+      var touch;
+      if (event.touches) {
+        touch = event.touches[0];
+      } else {
+        touch = event;
+      }
+      absX = touch.clientX - curX;
+      absY = touch.clientY - curY;
+      limX = nodeX + absX;
+      limY = nodeY + absY;
+      limX = limt(limX, 0, maxW);
+      limY = limt(limY, 0, maxH);
+      node.style.left = limX + "px";
+      node.style.top = limY + "px";
+      document.body.addEventListener('touchmove', function (event) {
+        event.preventDefault();
+      }, false)
+    }
+  }
+
+  function limt(cur, min, max) {
+    if (cur < min) {
+      return min;
+    } else if (cur >= max) {
+      return max;
+    } else {
+      return cur;
+    }
+  }
+
+  function end() {
+    flag = false;
+  }
+  node.addEventListener("mousedown", function () {
+    down();
+  }, false);
+  node.addEventListener("touchstart", function () {
+    down();
+  }, false)
+  node.addEventListener("mousemove", function () {
+    move();
+  }, false);
+  node.addEventListener("touchmove", function () {
+    move();
+  }, false)
+  document.body.addEventListener("mouseup", function () {
+    end();
+  }, false);
+  node.addEventListener("touchend", function () {
+    end();
+  }, false);
+}
